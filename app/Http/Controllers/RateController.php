e2ac59fd5a8e6230b734d2c380dc8a55ad34f367
Kaleffdev <markskalijevs@gmail.com>
2022-09-12 23:37:21 +0300

    Created model, migration and api, console routes and cron job

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rate;
 use Illuminate\Http\Request;
 
 class RateController extends Controller
 {
-    //
+    // Store the rates data in database
+    public function store()
+    {
+        $xmlString = file_get_contents('http://www.bank.lv/vk/ecb.xml');
+        $xmlObject = simplexml_load_string($xmlString, 'SimpleXMLElement', LIBXML_NOCDATA);
+        // Convert xml data to json and back to PHP array
+        $json = json_encode($xmlObject);
+        $phpArray = json_decode($json, true);
+        Rate::upsert($phpArray['Currencies']['Currency'], ['ID'], ['rate']);
+    }
+
+    public function index()
+    {
+        // Return the information about all the currencies
+        $rates = Rate::all()
+                    ->pluck('rate', 'ID');
+        return response()->json($rates);
+    }
 }
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\RateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('updaterates', function(RateController $rateController) {
+    $this->info('Scraping data..');
+    $rateController->store();
+    $this->info('Data succesfully stored!');
+})->purpose('Update the currency rates');
